1a90bb1179d08762325f21f55fb98bb9943b6539
lyl <liuyulong@defensedriver.com.cn>
2025-12-11 10:11:16 +0800

    modified:   .gitignore

--- refs/com/hiklife/svc/core/protocol/initialisation/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.hiklife.svc.core.protocol.initialisation;
-
-
-/* Location:              /Users/liu/Documents/工作文档/珑琪/智能盒子/kvm/svc-console-master/lib/svc-sdk-1.3.4.1/!/com/hiklife/svc/core/protocol/initialisation/package-info.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
